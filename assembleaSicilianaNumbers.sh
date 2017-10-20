d2d755a526d491f5263353153495a77843519a3f
aborruso <aborruso@gmail.com>
2017-10-20 12:04:15 +0200

    update

--- assembleaSicilianaNumbers.sh
+++ assembleaSicilianaNumbers.sh
@@ -24,7 +24,7 @@ do
   indirizziDeputatii=($(curl -s "$uri" | pup '#myTable > tbody > tr > td:nth-child(2) > a[href*="scheda.jsp"] attr{href}'))
   idGruppo=$(echo "$m" | sed -r 's/^.*dGruppo=//g')
 # estraggo i nomi dei gruppi
-  curl -s "$uri" | scrape -be '//td[@id="pageContent"]' | xml2json | jq -r '.html.body.td.div | ['"$idGruppo"',."$t"] | @csv' >> ./anagraficaGruppi.csv
+  curl -s "$uri" | scrape -be '//td[@id="pageContent"]' | xml2json | jq -r '.html.body.td.div | ['"$idGruppo"',."$t"] | @csv' >> ./tmp_anagraficaGruppi00.csv
 # estraggo l'anagrafica dei deputati
   curl -s "$uri" | scrape -be '//td[@id="pageContent"]' | xml2json | jq -r '.html.body.td.table[1].tr[] | ['"$idGruppo"',.td[1].a."$t",.td[3]."$t",(.td[1].a.href | tostring | gsub("^.*Deputato="; ""))] | @csv' >> ./tmp_anagraficaDeputati.csv
 # per ogni pagina di un deputato estraggo il numero di attività svolte da primo firmatario e da cofirmatario
@@ -54,7 +54,11 @@ csvsql -I --query 'select idDeputato,idGruppo,nome,collegio from tmp_anagraficaD
 csvsql -I --query 'select idDeputato,nome,collegio from tmp_anagraficaDeputati_01' ./tmp_anagraficaDeputati_01.csv > ./tmp_anagraficaDeputati_02.csv
 
 # aggiungo la riga di intestazione a anagraficaGruppi
-sed -i '1s/^/idGruppo,nomeGruppo\n/' ./anagraficaGruppi.csv
+sed -i '1s/^/idGruppo,nomeGruppo\n/' ./tmp_anagraficaGruppi00.csv
+
+# rimuovo eventuali gruppi che in anagrafica non hanno il nomeGruppo valorizzato
+csvsql -I --query "select * FROM tmp_anagraficaGruppi00 where nomeGruppo is not null order by idGruppo" ./tmp_anagraficaGruppi00.csv > ./anagraficaGruppi.csv
+cat ./anagraficaGruppi.csv > ./tmp_anagraficaGruppi00.csv
 
 # faccio il join tra la tabella dei dati sui primi firmatari e quella dei cofirmatari e la riordino 
 csvjoin -I -c 1 primofirmatario.csv cofirmatario.csv > tmp_attivitaDeputatiRegioneSiciliana00.csv
@@ -64,7 +68,7 @@ csvsql -I --query "select * from tmp_attivitaDeputatiRegioneSiciliana00 order by
 csvjoin -I -c 1 ./tmp_attivitaDeputatiRegioneSiciliana.csv ./tmp_anagraficaDeputati_02.csv > tmp_attivitaDeputatiRegioneSiciliana_02.csv
 
 # sposto nomeGruppo di anagraficaGruppi a inizio tabella
-csvsql -I --query "select distinct nomeGruppo,idGruppo from anagraficaGruppi" anagraficaGruppi.csv > tmp_anagraficaGruppi.csv
+csvsql -I --query "select distinct nomeGruppo,idGruppo from tmp_anagraficaGruppi00" tmp_anagraficaGruppi00.csv > tmp_anagraficaGruppi.csv
 
 # aggiungo l'anagrafica dei gruppi ai dati sui firmatari
 csvjoin -I -c 2 ./tmp_attivitaDeputatiRegioneSiciliana_02.csv ./tmp_anagraficaGruppi.csv > tmp_attivitaDeputatiRegioneSiciliana_03.csv
@@ -73,7 +77,7 @@ csvjoin -I -c 2 ./tmp_attivitaDeputatiRegioneSiciliana_02.csv ./tmp_anagraficaGr
 csvsql -I --query "select *, 'http://www.ars.sicilia.it/DocumentiEsterni/Immagini/Deputati/'||substr('000000'||idDeputato, -6, 6)||'.jpg' as URLfoto from tmp_attivitaDeputatiRegioneSiciliana_03" ./tmp_attivitaDeputatiRegioneSiciliana_03.csv > ./tmp_attivitaDeputatiRegioneSiciliana_04.csv
 
 # rimuovo i deputati doppioni raggruppando per idDeputato
-csvsql -I --query "select * FROM tmp_attivitaDeputatiRegioneSiciliana_04 group by idDeputato" ./tmp_attivitaDeputatiRegioneSiciliana_04.csv > ./attivitaDeputatiRegioneSiciliana.csv
+csvsql -I --query "select * FROM tmp_attivitaDeputatiRegioneSiciliana_04 group by idDeputato order by idDeputato" ./tmp_attivitaDeputatiRegioneSiciliana_04.csv > ./attivitaDeputatiRegioneSiciliana.csv
 
 # rimuovo la stringa 'Gruppo dalla colonna nomeGruppo'
 sed -i 's/,Gruppo /,/g' attivitaDeputatiRegioneSiciliana.csv
